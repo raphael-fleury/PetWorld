35013c7e2075dc97db514ffe4a36cd130b37a1bf
rfleury-fcamara <54419491+raphael-fleury@users.noreply.github.com>
2022-03-13 15:21:05 -0300

    Express installed

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello world')
+import express from "express";
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+app.listen(port, () => console.log('Listening on port ' + port));
