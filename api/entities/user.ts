4dc50d06522140e76f5e83cba59124b76bee31a4
rfleury-fcamara <54419491+raphael-fleury@users.noreply.github.com>
2022-03-13 22:11:10 -0300

    Encrypting passwords

--- api/entities/user.ts
+++ api/entities/user.ts
@@ -1,5 +1,6 @@
 import validator from "validator";
 import { prop } from "@typegoose/typegoose";
+import { encrypt } from "../encrypt";
 
 export default class User {
     @prop({ required: true })
@@ -14,8 +15,8 @@ export default class User {
 
     @prop({
         required: true,
-        minlength: [ 8, "Password needs at least 8 characters"],
-        maxlength: [ 24, "Password needs at shorter than 25 characters"]
+        minlength: [ 8, "Password must be at least 8 characters long"],
+        set: encrypt
     })
     public password!: string
 }
