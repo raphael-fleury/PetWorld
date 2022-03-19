6b4e1219752a7c846ae2f24144d095cb8c37d699
rfleury-fcamara <54419491+raphael-fleury@users.noreply.github.com>
2022-03-19 17:06:41 -0300

    Handling validation errors

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -1,5 +1,16 @@
+import User from "entities/user";
 import { Application } from "express"
 import { userSchema } from "../entities/schemas"
+
+function treatValidationError(error) {
+    const errors: any[] = [];
+
+    Object.keys(error.errors).forEach(key => {
+        errors.push(error.errors[key].properties);
+    })
+
+    return errors;
+}
 
 export default (app: Application) => {
     app.post('/users', async (req, res) => {
@@ -8,21 +19,30 @@ export default (app: Application) => {
             res.status(200).send(user);
         }
         catch (error: any) {
+            if (error.name === "ValidationError")
+                return res.status(400).send(treatValidationError(error));
+
             res.status(500).send(error.message);
         }
     })
     
     app.put('/users/:id', async (req, res) => {
         try {
-            console.log(req.body);
             const user = await userSchema.findById(req.params.id);
-            if (!user) return res.status(400).send('User not found.');
-            user.password = req.body.password;
+            if (!user)
+                return res.status(404).send('User not found.');
+
+            Object.keys(req.body).forEach(key => {
+                user[key] = req.body[key];
+            });
+            
             await user.save();
-    
             res.status(200).send(user);
         }
         catch (error: any) {
+            if (error.name === "ValidationError")
+                return res.status(400).send(treatValidationError(error));
+
             res.status(500).send(error.message);
         }
     })
